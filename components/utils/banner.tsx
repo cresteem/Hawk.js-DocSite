aa09c58530031a5ef4585def5f760a04ae60cd5f
DARSAN <iamspdarsan@gmail.com>
2024-11-29 01:05:05 +0530

    routes updated, banner and base util components added

---
+++ components/utils/banner.tsx
@@ -0,0 +1,12 @@
+export default () => {
+  return (
+    <a
+      href="https://github.com/cresteem/Hawk.js/releases/latest"
+      target="_blank"
+      className="tracking-widest font-normal"
+    >
+      Hawk.js 1.5.0 released 🚀{" "}
+      <span className="underline text-deep">Explore</span>
+    </a>
+  );
+};
