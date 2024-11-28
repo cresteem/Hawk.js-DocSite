384132ad077530d72378d97f63d6097c8740412d
DARSAN <iamspdarsan@gmail.com>
2024-11-29 00:46:26 +0530

    favicon added and compressed, deps updated, migrated to nextra v3, build wf updated, tailwindcss added.

---
+++ pages/exploring-hawk-js-api-functions/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  hawk: {
+    title: "hawk ( )",
+  },
+  hawkstrategy: {
+    title: "hawkStrategy { }",
+  },
+  utils: {
+    title: "utilities { }",
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#008080",
+        deep: "#006666",
+      },
+    },
+  },
+  plugins: [],
+};
